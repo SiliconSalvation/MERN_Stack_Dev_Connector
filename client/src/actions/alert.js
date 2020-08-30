cce067c078e876b285443f63e8bd592535634c48
Nick S <staunw01@pfw.edu>
2020-08-29 23:49:25 -0400

    Finished section 8

--- client/src/actions/alert.js
+++ client/src/actions/alert.js
@@ -8,4 +8,4 @@ export const setAlert = (msg, alertType, timeout = 5000) => dispatch => {
         payload: { msg, alertType, id }
     });
     setTimeout(() => dispatch({ type: REMOVE_ALERT, payload: id }), timeout);
-}
+};
--- client/src/components/auth/Register.js
+++ client/src/components/auth/Register.js
@@ -1,13 +1,14 @@
 import React, { Fragment, useState } from 'react';
 import { connect } from 'react-redux';
-import { Link } from 'react-router-dom';
+import { Link, Redirect } from 'react-router-dom';
 import { setAlert } from '../../actions/alert';
-import PropTypes from 'prop-types'
+import { register } from '../../actions/auth';
+import PropTypes from 'prop-types';
 
 
 // import axios from 'axios';
 
-const Register = ({ setAlert }) => {
+const Register = ({ setAlert, register, isAuthenticated }) => {
     const [formData, setFormData] = useState({
         name: '',
         email: '',
@@ -24,7 +25,7 @@ const Register = ({ setAlert }) => {
         if(password !== password2) {
             setAlert('Passwords do not match', 'danger');
         } else{
-            console.log('Success');
+            register({ name, email, password });
         //     const newUser = {
         //         name,
         //         email,
@@ -49,6 +50,10 @@ const Register = ({ setAlert }) => {
          }
     };
 
+    if(isAuthenticated) {
+        return <Redirect to='/dashboard' />
+    }
+
     return  (
         <Fragment>
             <h1 className='large text-primary'>Sign Up</h1>
@@ -63,7 +68,7 @@ const Register = ({ setAlert }) => {
                         name='name' 
                         value={name} 
                         onChange={e => onChange(e)} 
-                        required 
+                        //required 
                     />
                 </div>
                 <div className='form-group'>
@@ -73,7 +78,7 @@ const Register = ({ setAlert }) => {
                         name='email'
                         value={email}
                         onChange={e => onChange(e)}
-                        required
+                        //required
                     />
                     <small className='form-text'
                         >This site uses Gravatar so if you want a profile image, use a
@@ -85,7 +90,7 @@ const Register = ({ setAlert }) => {
                         type='password'
                         placeholder='Password'
                         name='password'
-                        minLength='8'
+                        //minLength='8'
                         value={password}
                         onChange={e => onChange(e)}
                     />
@@ -95,7 +100,7 @@ const Register = ({ setAlert }) => {
                         type='password'
                         placeholder='Confirm Password'
                         name='password2'
-                        minLength='8'
+                        //minLength='8'
                         value={password2}
                         onChange={e => onChange(e)}
                     />
@@ -111,6 +116,12 @@ const Register = ({ setAlert }) => {
 
 Register.propTypes = {
     setAlert: PropTypes.func.isRequired,
+    register: PropTypes.func.isRequired,
+    isAuthenticated: PropTypes.bool,
 };
 
-export default connect(null, { setAlert })(Register);
+const mapStateToProps = state => ({
+    isAuthenticated: state.auth.isAuthenticated
+});
+
+export default connect(mapStateToProps, { setAlert, register })(Register);
